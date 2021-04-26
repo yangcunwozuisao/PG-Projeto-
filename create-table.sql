39e0a373b1f03b44ef83f2ba23e99ab16a73ebe5
Tomaz <tmsski@yahoo.com.br>
2021-04-26 19:11:30 -0300

    Versão inicial

---
+++ create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE professores (
+  id bigint(20) NOT NULL,
+  area varchar(255),
+  matricula int(11) NOT NULL,
+  nome varchar(255),
+  PRIMARY KEY (id)
+);
---
+++ src/main/java/ps2/restapidb/ProfessorRepo.java
@@ -0,0 +1,6 @@
+package ps2.restapidb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProfessorRepo extends CrudRepository<Professor, Long> {
+}
